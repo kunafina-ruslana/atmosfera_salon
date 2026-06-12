fee215383855a7b77faeda9e27e3e70569b6fe02
NOruslana <kunafina_ruslana@mail.ru>
2026-06-12 19:11:10 +0300

    update app

--- server/app.js
+++ server/app.js
@@ -41,10 +41,11 @@ const worksDir = path.join(__dirname, 'uploads', 'works');
 const promotionsDir = path.join(__dirname, 'uploads', 'promotions');
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: [
+    'http://localhost:3000',
+    'https://atmosfera-uili.onrender.com'
+  ],
+  credentials: true
 }));
 
 app.use(express.json({ limit: '50mb' }));
